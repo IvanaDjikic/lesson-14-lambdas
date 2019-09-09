9459209501bc45410d0a4434a28fa38ed8a46a40
IvanaDjikic <ivdjikic@gmail.com>
2019-09-09 18:33:30 +0200

    jobcandidate change

--- src/exercises/_3_JobCandidateSelectorUsingLambdas.java
+++ src/exercises/_3_JobCandidateSelectorUsingLambdas.java
@@ -1,14 +1,26 @@
 package exercises;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class _3_JobCandidateSelectorUsingLambdas {
 	public static void main(String[] args) {
 
 		// 1. Sort the candidates by salary requirements, low to high.
-		
-
+		Collections.sort(JobCandidate.jobCandidates, (jc1, jc2) -> jc1.getSalaryRequired() - jc2.getSalaryRequired());
+		System.out.println(JobCandidate.jobCandidates);
 		// 2. Filter out any candidates that have not been taught by me (June)
+		Stream<JobCandidate> streamOfCandidates = JobCandidate.jobCandidates.stream();
+
+		Predicate<JobCandidate> predicate1 = jc -> jc.getTeacherName().contains("June")
+				|| jc.getTeacherName().contains("Joon");
+		List<JobCandidate> candidatesTaughtByJune = streamOfCandidates.filter(predicate1).collect(Collectors.toList());
+		System.out.println(candidatesTaughtByJune);
+
 		// Hint: see the removeIf() method in the Collections class
-		
-		
+
 	}
 }
